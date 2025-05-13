05997833fff583ad57126108d99b9c676eb4d937
Josh <schultzjosh0@gmail.com>
2025-05-13 02:31:03 -0400

    fixed list handling

--- hw13.rkt
+++ hw13.rkt
@@ -16,10 +16,17 @@
   (lm (q)
     (1st (rst (rst q)))))
 
+;; length : List -> Integer
+(bind/rec length
+  (lm (lst)
+    (iffy (~= lst mt)
+          0
+          (+ 1 (length (rst lst))))))
+
 ;; all? : (Any->Boolean) List -> Boolean
 (bind/rec all?
   (lm (pred lst)
-    (iffy (~= (length lst) 0)
+    (iffy (~= lst mt)
           (> 1 0)
           (∧ (pred (1st lst))
              (all? pred (rst lst))))))
@@ -27,7 +34,7 @@
 ;; any? : (Any->Boolean) List -> Boolean
 (bind/rec any?
   (lm (pred lst)
-    (iffy (~= (length lst) 0)
+    (iffy (~= lst mt)
           (> 0 1)
           (∨ (pred (1st lst))
              (any? pred (rst lst))))))
@@ -41,9 +48,9 @@
 ;; has-dup? : List -> Boolean
 (bind/rec has-dup?
   (lm (lst)
-    (iffy (~= (length lst) 0)
+    (iffy (~= lst mt)
           (> 0 1)
-          (iffy (~= (length (rst lst)) 0)
+          (iffy (~= (rst lst) mt)
                 (> 0 1)
                 (∨ (has? (1st lst) (rst lst))
                    (has-dup? (rst lst)))))))
@@ -56,51 +63,43 @@
      (∨ (~= (qx q1) (qx q2))
         (~= (qy q1) (qy q2))
         (~= (abs (+ (qx q1) (× (qx q2) -1)))
-           (abs (+ (qy q1) (× (qy q2) -1))))))))
+            (abs (+ (qy q1) (× (qy q2) -1))))))))
 
 ;; safe-to-add? : Queen ListofQueen -> Boolean
 (bind/rec safe-to-add?
   (lm (q qs)
-    (all? (lm (o) (safe? q o)) qs)))
+    (all? (lm (o) (safe? q o))
+          qs)))
 
 ;; all-safe? : ListofQueen -> Boolean
 (bind/rec all-safe?
   (lm (qs)
-    (iffy (~= (length qs) 0)
+    (iffy (~= qs mt)
           (> 1 0)
-          (iffy (~= (length (rst qs)) 0)
+          (iffy (~= (rst qs) mt)
                 (> 1 0)
                 (∧ (safe-to-add? (1st qs) (rst qs))
-                   (all-safe?    (rst qs)))))))
-
-
-;; length : List -> Integer
-(bind/rec length
-  (lm (lst)
-    (iffy (~= lst mt)
-          0
-          (+ 1 (length (rst lst))))))
-
+                   (all-safe? (rst qs)))))))
 
 ;; nqueens : Int -> ListofQ or 'FALSE!
 (bind/rec nqueens
   (lm (n)
     (bind/rec [helper
-               (lm (row acc)
-                 (iffy (> row n)
-                       acc
-                       (bind/rec [try-col
-                                  (lm (col)
-                                    (iffy (> col n)
-                                          (> 0 1)
-                                          (bind [cand (Q row col)]
-                                            (bind [res (helper
-                                                        (+ row 1)
-                                                        (cns cand acc))]
-                                              (iffy (~= res (> 0 1))
-                                                    res
-                                                    (try-col (+ col 1)))))))]
-                         (try-col 1))))]
+                (lm (row acc)
+                  (iffy (> row n)
+                        acc
+                        (bind/rec [try-col
+                                   (lm (col)
+                                     (iffy (> col n)
+                                           (> 0 1)
+                                           (bind [cand (Q row col)]
+                                             (bind [sol (helper
+                                                         (+ row 1)
+                                                         (cns cand acc))]
+                                               (iffy (~= sol (> 0 1))
+                                                     sol
+                                                     (try-col (+ col 1)))))))]
+                             (try-col 1))))]
       (helper 1 mt))))
 
 
